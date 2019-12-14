e98a0b95957df164b11add878d9b716f9a42ae25
Steve Roth <steve.roth@hpe.com>
2019-12-13 20:38:00 -0800

    Bug fix in door.

--- door/src/orderToFormData.js
+++ door/src/orderToFormData.js
@@ -8,12 +8,12 @@ export default function (o) {
     fd.append(`${prefix}.product`, ol.product)
     fd.append(`${prefix}.quantity`, ol.quantity)
     fd.append(`${prefix}.price`, ol.price)
-    fd.append(`${prefix}.used`, ol.used)
-    fd.append(`${prefix}.usedAt`, ol.usedAt)
+    if (ol.used) fd.append(`${prefix}.used`, ol.used)
+    if (ol.usedAt) fd.append(`${prefix}.usedAt`, ol.usedAt)
   })
   fd.append('payment1.type', o.payments[0].type)
-  fd.append('payment1.subtype', o.payments[0].subtype)
-  fd.append('payment1.method', o.payments[0].method)
+  if (o.payments[0].subtype) fd.append('payment1.subtype', o.payments[0].subtype)
+  if (o.payments[0].method) fd.append('payment1.method', o.payments[0].method)
   fd.append('payment1.amount', o.payments[0].amount)
   return fd
 }
