3a9184fdfad474694f4b212c03f069006ec4fabe
Steve Roth <steve@rothskeller.net>
2022-03-10 10:01:21 -0800

    Add leave blank note on gala form.

--- payment-forms/src/gala/GalaRegisterGuest.vue
+++ payment-forms/src/gala/GalaRegisterGuest.vue
@@ -3,13 +3,42 @@ GalaRegisterGuest handles the registration form contents for a single guest.
 -->
 
 <template lang="pug">
-b-form-group(:label="`Guest #${number+1}`" label-class="font-weight-bold")
-  b-form-group(label="Name" :label-for="`guest-${number}-name`" label-cols-sm="auto" label-class="gala-guest-label")
-    b-form-input(:id="`guest-${number}-name`" trim v-model="name")
-  b-form-group(label="Email" :label-for="`guest-${number}-email`" label-cols-sm="auto" label-class="gala-guest-label" :state="emailError ? false : null" :invalid-feedback="emailError")
-    b-form-input(:id="`guest-${number}-email`" lazy trim v-model="email" :state="emailError ? false : null")
-  b-form-group(label="Entree" :label-for="`guest-${number}-entree`" label-cols-sm="auto" label-class="gala-guest-label")
-    b-form-select(:id="`guest-${number}-entree`" :options="entreeOptions" v-model="entree")
+b-form-group
+  legend.bv-no-focus-ring.col-form-label.pt-0.font-weight-bold(slot='label', tabindex='-1')
+    | Guest {{ number + 1 }}
+    span(
+      v-if='number !== 0',
+      style='margin-left: 1em; font-weight: normal; color: #888; font-size: 0.875rem'
+    ) (leave blank if not known yet)
+  b-form-group(
+    label='Name',
+    :label-for='`guest-${number}-name`',
+    label-cols-sm='auto',
+    label-class='gala-guest-label'
+  )
+    b-form-input(:id='`guest-${number}-name`', trim, v-model='name')
+  b-form-group(
+    label='Email',
+    :label-for='`guest-${number}-email`',
+    label-cols-sm='auto',
+    label-class='gala-guest-label',
+    :state='emailError ? false : null',
+    :invalid-feedback='emailError'
+  )
+    b-form-input(
+      :id='`guest-${number}-email`',
+      lazy,
+      trim,
+      v-model='email',
+      :state='emailError ? false : null'
+    )
+  b-form-group(
+    label='Entree',
+    :label-for='`guest-${number}-entree`',
+    label-cols-sm='auto',
+    label-class='gala-guest-label'
+  )
+    b-form-select(:id='`guest-${number}-entree`', :options='entreeOptions', v-model='entree')
 </template>
 
 <script>
